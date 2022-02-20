ca1b79d3e1e53bd14babd7a5138c58e069daea57
PastMoments <3858420+PastMoments@users.noreply.github.com>
2022-02-19 19:56:14 -0500

    Fixed table display

--- Stormhacks2022/flaskweb.py
+++ Stormhacks2022/flaskweb.py
@@ -3,17 +3,23 @@ from werkzeug.utils import secure_filename
 import os
 import pandas as pd
 import re
+import sys
+import numpy as np
 
 UPLOAD_FOLDER = './uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_53Ff3"F4QFdz\n\xec]/'
+app.config['SESSION_TYPE'] = 'filesystem'
 var = None
 
 @app.route("/")
 def home():
-    if session.get("transactions"):
-        df = session.get("transactions")
-        return render_template('home.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    df_json = session.get("transactions")
+    if df_json:
+        df = pd.read_json(df_json, dtype=True)
+        df = df.fillna(value=np.nan)
+        return render_template('home.html', tables=[df.to_html(classes='data', na_rep='')], titles=df.columns.values)
     else:
         return render_template('home.html')
 
@@ -21,10 +27,18 @@ def home():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        df = load_file()
-        session["transactions"] = df
+        if f.filename:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+            f.save(filename)
+
+            # column names since the bank statements don't include them by default
+            colnames=['TIMESTAMP', 'DESCRIPTION', 'WITHDRAWALS', 'DEPOSITS', 'BALANCE']
+            df = pd.read_csv(path, sep=',', header=None, names=colnames)
+            add_categories(df)
+
+            session["transactions"] = df.to_json()
+        else:
+            session["transactions"] = ""
     return redirect(url_for('home'))
 
 def category_map(description):
@@ -46,25 +60,8 @@ def add_categories(data_frame):
     data_frame["CATEGORIES"] = data_frame["DESCRIPTION"].map(category_map)
 
 
-def load_file():
-    # changes directory to uploads folder
-    ref_dir = os.getcwd()
-    curr_dir = os.chdir(ref_dir + "/uploads/")
-
-    files = os.listdir(os.getcwd())
-    recent_file = max(files, key=os.path.getctime)
-
-    # column names since the bank statements don't include them by default
-    # colnames=['TIMESTAMP', 'DESCRIPTION', 'WITHDRAWALS', 'DEPOSITS']
-    colnames=['TIMESTAMP', 'DESCRIPTION', 'AMOUNT']
-    df = pd.read_csv('test.csv', sep=',', header=None, names=colnames)
-    add_categories(df)
-    return df
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-    app.secret_key = 'any random string'
 
 
 
